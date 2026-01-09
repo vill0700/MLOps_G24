7c541340375b43cc4c3742ee1e0e683cc4f8f46a
s246035(villads) <v.dehli@gmail.com>
2026-01-09 15:28:38 +0100

    Fixed Numpy dependency setup initial model

--- src/mlopsg24/model.py
+++ src/mlopsg24/model.py
@@ -1,17 +1,31 @@
 import torch
 import torch.nn as nn
 
-class JobAdClassifier(nn.Module):
-    def __init__(self, input_dim=768, num_classes=20):
+device = torch.device(
+    "cuda" if torch.cuda.is_available()
+    else "mps" if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Using device: {device}")
+
+#Neural Network for Job Advertisement Classification
+class NeuralNetwork(nn.Module):
+    def __init__(self,input_size=768, num_classes=20):
         super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(input_dim, 512),
+        self.flatten = nn.Flatten()
+
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(input_size, 512),
             nn.ReLU(),
-            nn.Linear(512, 256),
+            nn.Linear(512, 512),
             nn.ReLU(),
-            nn.Linear(256, num_classes)
+            nn.Linear(512, num_classes),
         )
 
     def forward(self, x):
-        # x: (batch, 768)
-        return self.net(x)  # logits: (batch, 20)
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+model = NeuralNetwork().to(device)
+print(model)
