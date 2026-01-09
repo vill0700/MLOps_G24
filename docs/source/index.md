0fc8c6b986eef9f3065e54dc595ac4cc9103dd13
s246035(villads) <v.dehli@gmail.com>
2026-01-09 10:55:04 +0100

    Project cookie setup

---
+++ docs/source/index.md
@@ -0,0 +1,3 @@
+## Documentation
+
+Documentation for mlopsg24
---
+++ src/mlopsg24/train.py
@@ -0,0 +1,10 @@
+from mlopsg24.model import Model
+from mlopsg24.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
