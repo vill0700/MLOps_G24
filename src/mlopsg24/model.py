084efc1f8377e9425bb32e9df55bb846fcf61d31
aut0mat0n <v.dehli@gmail.com>
2026-01-15 12:00:20 +0100

    #

--- src/mlopsg24/model.py
+++ src/mlopsg24/model.py
@@ -6,10 +6,7 @@ class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
         self.model = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(784, 256),
-            nn.ReLU(),
-            nn.Linear(256, 128),
+            nn.Linear(784, 128),
             nn.ReLU(),
             nn.Linear(128, 64),
             nn.ReLU(),
